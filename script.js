42943fd020a0ececd1de4791ff31448be1e259c4
tynchtyk <tynchtyktabyldiev@gmail.com>
2022-05-17 19:23:52 +0600

    pagess

---
+++ script.js
@@ -0,0 +1,23 @@
+// let tynchtyk = {
+//     name:'tynchtyk',
+//     age: 20,
+//     hobbes:{
+//         bir: 'playfootball',
+//         eki: 'sing song',
+//     }
+// }
+// console.log(tynchtyk)
+// console.log(Object.keys(tynchtyk))
+// console.log(Object.values(tynchtyk))
+
+
+//функции
+let button = document.querySelector('.btn')
+button.addEventListener('click',func)
+ let p =document.getElementById('p')
+function func (){
+    alert('мени бастын')
+    p.style.fontSize = '50px'
+}
+
+
